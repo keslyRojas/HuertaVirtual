80675be76960d3589349785ce2c4099b33149da0
anamirandavillalobos <ana.agmv.agmv@gmail.com>
2025-12-01 16:20:59 -0600

    cambios

---
+++ resources/views/game/game.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Huerta</title>
+
+    <link rel="stylesheet" href="{{ asset('css/huerta.css') }}">
+</head>
+<body>
+
+
+    <div class="top-bar">
+        
+        <div class="coins-box">
+            <img src="/img/coin.png" class="coin">
+            <img src="/img/coin.png" class="coin coin2">
+        </div>
+
+        <div class="top-icons">
+            <button class="icon-btn">
+                <img src="/img/bell.png" class="icon-img">
+            </button>
+
+            <button class="icon-btn">
+                <img src="/img/stats.png" class="icon-img">
+            </button>
+        </div>
+
+    </div>
+
+
+    <div class="main-box">
+        <div class="inner-box"></div>
+        <div class="inner-box"></div>
+    </div>
+
+
+    <div class="action-buttons">
+
+        <button class="img-btn">
+            <img src="/img/water-btn.png" alt="Water">
+        </button>
+
+        <button class="img-btn">
+            <img src="/img/plant-btn.png" alt="Plant">
+        </button>
+
+        <button class="img-btn">
+            <img src="/img/harvest-btn.png" alt="Harvest">
+        </button>
+
+        <button class="img-btn">
+            <img src="/img/fertilizer-btn.png" alt="Fertilize">
+        </button>
+
+    </div>
+
+</body>
+</html>
